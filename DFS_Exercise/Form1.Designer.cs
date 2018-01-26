4138843fc58b5f987e0aeab7457327757d2d1f73
AlbinoShadow <joestgermain@msn.com>
2018-01-25 22:48:57 -0500

    Initial Commit

---
+++ DFS_Exercise/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace DFS_Exercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.pomodoro = new System.Windows.Forms.Label();
+            this.breakLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(21, 185);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(102, 185);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 1;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(183, 185);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // pomodoro
+            // 
+            this.pomodoro.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pomodoro.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pomodoro.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pomodoro.Location = new System.Drawing.Point(102, 32);
+            this.pomodoro.Name = "pomodoro";
+            this.pomodoro.Size = new System.Drawing.Size(75, 32);
+            this.pomodoro.TabIndex = 3;
+            this.pomodoro.Text = "25:00";
+            this.pomodoro.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // breakLabel
+            // 
+            this.breakLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.breakLabel.Location = new System.Drawing.Point(0, 0);
+            this.breakLabel.Name = "breakLabel";
+            this.breakLabel.Size = new System.Drawing.Size(284, 23);
+            this.breakLabel.TabIndex = 4;
+            this.breakLabel.Text = "Click \"Start\" to start the timer!";
+            this.breakLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.breakLabel);
+            this.Controls.Add(this.pomodoro);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pomodoro Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label pomodoro;
+        private System.Windows.Forms.Label breakLabel;
+    }
+}
+
---
+++ DFS_Exercise/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DFS_Exercise
+{
+    public partial class Form1 : Form
+    {
+        Random randomBreakTime = new Random();  // Random break time to allow breaks between 3-5 mins and 15-30 mins.
+        int timeLeft;                           // Time left in seconds.
+        int breakNum = 0;                       // Number of breaks taken so far.
+        string timeRemaining;                   // Time remaining in string form for the stopwatch label.
+        bool breakTime = false;                 // Boolean to determine whether or not it's break time.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Timer ticks every 1000 ms.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // While the timer is running:
+            if (timeLeft > 0)
+            {
+                timeLeft -= 1;
+
+                if (timeLeft % 60 >= 10)
+                    timeRemaining = (timeLeft / 60).ToString() + ":" + (timeLeft % 60).ToString();
+                else
+                    timeRemaining = (timeLeft / 60).ToString() + ":0" + (timeLeft % 60).ToString();
+
+                pomodoro.Text = timeRemaining;
+            }
+            // If the timer has hit zero:
+            else
+            {
+                if (breakNum < 4 && breakTime == false)
+                {
+                    breakTime = true;
+                    timeLeft = randomBreakTime.Next(180, 300);  // Random break between 3 and 5 minutes.
+                    MessageBox.Show("Time for a break!", "Times up!");
+                    breakLabel.Text = "You should be taking a break!";
+                }
+                else if (breakTime == false)
+                {
+                    breakTime = true;
+                    timeLeft = randomBreakTime.Next(900, 1800); // Random break between 15 and 30 minutes.
+                    MessageBox.Show("Time for a break!", "Times up!");
+                    breakLabel.Text = "You should be taking a break!";
+                }
+                else
+                {
+                    timeLeft = 1500;
+                    breakTime = false;
+                    MessageBox.Show("Time to get back to work!", "Time to work!");
+                    breakLabel.Text = "You should be working!";
+                }
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            timeLeft = 1500;    // 25 minutes in seconds is 1500.
+            timer1.Start();
+            startButton.Text = "Reset";
+            breakLabel.Text = "You should be working!";
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                timer1.Stop();
+                pauseButton.Text = "Resume";
+            }
+            else
+            {
+                timer1.Start();
+                pauseButton.Text = "Pause";
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            startButton.Text = "Start";
+            timeLeft = 1500;            // Set the clock to 25mins.
+            pomodoro.Text = "25:00";
+            timer1.Stop();
+        }
+    }
+}
